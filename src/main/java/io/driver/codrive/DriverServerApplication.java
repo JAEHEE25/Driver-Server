4b1229b6ec48d2e3f603549aa23c750d46eba192
jaehee <y2hjjh@naver.com>
2024-06-25 16:16:22 +0900

    Initial Commit

---
+++ src/main/java/io/driver/codrive/DriverServerApplication.java
@@ -0,0 +1,13 @@
+package io.driver.codrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/driver/codrive/DriverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.driver.codrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
