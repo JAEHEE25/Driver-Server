52444f2f6abe15935f3739cf539281b502e34807
JAEHEE25 <y2hjjh@naver.com>
2024-07-28 21:30:56 +0900

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:17
 CMD ["./gradlew", "clean", "build"]
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-jar", "./app.jar"]
--- src/test/java/io/driver/codrive/DriverServerApplicationTests.java
+++ src/test/java/io/driver/codrive/DriverServerApplicationTests.java
@@ -3,7 +3,7 @@ package io.driver.codrive;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DriverServerApplicationTests.class)
 class DriverServerApplicationTests {
 
 	@Test
