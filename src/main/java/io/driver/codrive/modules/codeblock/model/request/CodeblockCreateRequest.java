9d196868572bc3e52e8ab92184a44b818e56ae0d
JAEHEE25 <y2hjjh@naver.com>
2024-11-12 15:43:04 +0900

    [#108] 추가 기능 반영
    - 글자수 유효성 검사 추가

--- src/main/java/io/driver/codrive/modules/codeblock/model/request/CodeblockCreateRequest.java
+++ src/main/java/io/driver/codrive/modules/codeblock/model/request/CodeblockCreateRequest.java
@@ -5,14 +5,17 @@ import java.util.List;
 import io.driver.codrive.modules.codeblock.domain.Codeblock;
 import io.driver.codrive.modules.record.domain.Record;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record CodeblockCreateRequest(
 	@Schema(description = "코드", example = "code")
+	@Size(max = 15000, message = "코드는 {max}자 이하로 입력해주세요.")
 	String code,
 
 	@Schema(description = "메모", example = "memo")
+	@Size(max = 15000, message = "메모는 {max}자 이하로 입력해주세요.")
 	String memo
 ) {
 	public static List<Codeblock> of(List<CodeblockCreateRequest> requests, Record record) {
--- src/main/java/io/driver/codrive/modules/record/model/request/RecordModifyRequest.java
+++ src/main/java/io/driver/codrive/modules/record/model/request/RecordModifyRequest.java
@@ -18,16 +18,14 @@ import jakarta.validation.constraints.Size;
 public record RecordModifyRequest(
 	@Schema(description = "문제 풀이 제목", example = "문제 풀이 제목")
 	@NotBlank(message = "문제 풀이 제목을 입력해주세요.")
+	@Size(max = 250, message = "문제 풀이 제목은 {max}자 이하로 입력해주세요.")
 	String title,
 
 	@Schema(description = "난이도", example = "1")
 	@Range(min = 1, max = 5, message = "난이도는 {min}부터 {max}까지 선택해주세요.")
 	int level,
 
-	@Schema(description = "문제 유형 태그 (최대 2개)", example = "[\"완전탐색\"]",
-		allowableValues = {"해시", "스택/큐", "힙 (Heap)", "정렬", "완전탐색", "탐욕법 (Greedy)",
-			"동적계획법 (Dynamic Programming)", "깊이 우선 탐색 (DFS)", "너비 우선 탐색 (BFS)", "이분탐색",
-			"그래프", "트리", "투포인터"})
+	@Schema(description = "문제 유형 태그 (최대 2개)", example = "[\"완전탐색\"]")
 	@NotNull(message = "문제 유형 태그를 선택해주세요.")
 	@Size(min = 1, max = 2, message = "문제 유형 태그는 {min}개 이상 {max}개 이하로 선택해주세요.")
 	List<String> tags,
