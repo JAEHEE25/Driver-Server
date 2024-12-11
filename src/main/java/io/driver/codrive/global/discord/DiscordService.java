9af0600d7a89a916967c735ca03be8e05608b469
JAEHEE25 <y2hjjh@naver.com>
2024-12-11 15:22:49 +0900

    [#112] 리팩토링
    - discord, notification send 비동기 처리

--- src/main/java/io/driver/codrive/global/discord/DiscordService.java
+++ src/main/java/io/driver/codrive/global/discord/DiscordService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -20,11 +21,13 @@ public class DiscordService {
 
 	private final WebClient webClient = WebClient.create(webhookUrl);
 
-    public void sendMessage(DiscordEventMessage eventMessage, String nickname) {
+    @Async
+	public void sendMessage(DiscordEventMessage eventMessage, String nickname) {
         DiscordMessage discordMessage = DiscordMessage.of(eventMessage.getUserMessage(nickname));
         sendMessageToDiscord(discordMessage);
     }
 
+	@Async
     public void sendMessage(DiscordEventMessage eventMessage, String nickname, String groupTitle) {
         DiscordMessage discordMessage = DiscordMessage.of(eventMessage.getGroupMessage(nickname, groupTitle));
         sendMessageToDiscord(discordMessage);
